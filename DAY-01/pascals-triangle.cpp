4fcb9370c351b86e1cd40bef7dd80ad556d1e2c2
Naveen <chokkapunaveen260503@gmail.com>
2022-10-21 21:46:15 +0530

    done

---
+++ DAY-01/pascals-triangle.cpp
@@ -0,0 +1,19 @@
+/* https://leetcode.com/problems/pascals-triangle/ */
+
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans(n);
+        ans[0].push_back(1);
+        for(int i=1;i<n;i++) {
+            ans[i].push_back(1);
+            if(i>1) {
+                for(int j=1;j<ans[i-1].size();j++) {
+                    ans[i].push_back(ans[i-1][j-1]+ans[i-1][j]);
+                }
+            }
+            ans[i].push_back(1);
+        }
+        return ans;
+    }
+};
